b4c15674ff5f09333c72a249b13996b1cde68dfe
ugoetudo <uoe12341234@gmail.com>
2025-11-05 14:11:37 -0500

    minor code formatting changes

--- src/App.jsx
+++ src/App.jsx
@@ -6,19 +6,18 @@ import {BrowserRouter, Routes, Route, Link } from 'react-router';
 export function App() {
   return (
     <>
-    <div className="mainContent"> 
-    <h1>Healthy Habits Gym</h1> 
-    
-      <BrowserRouter>
-        <nav>
-          <Link to="/">Home</Link> | <Link to="/sampleForm">Sample Form</Link>
-        </nav>
-        <Routes>
-          <Route path="/" element={<NewExercise />} />
-          <Route path="/sampleForm" element={< MyForm/>}/>
-        </Routes>
-      </BrowserRouter>
-    </div>
+      <div className="mainContent"> 
+        <h1>Healthy Habits Gym</h1> 
+        <BrowserRouter>
+          <nav>
+            <Link to="/">Home</Link> | <Link to="/sampleForm">Sample Form</Link>
+          </nav>
+          <Routes>
+            <Route path="/" element={<NewExercise />} />
+            <Route path="/sampleForm" element={< MyForm/>}/>
+          </Routes>
+        </BrowserRouter>
+      </div>
     </>
   );
 }
